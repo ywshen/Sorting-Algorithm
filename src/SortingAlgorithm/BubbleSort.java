b8c26ddce5ca112e895f2533f288cdd23bc220da
ywshen <ywshen5002@gmail.com>
2020-05-11 18:33:13 -0400

    Bubble Sort

---
+++ src/SortingAlgorithm/BubbleSort.java
@@ -0,0 +1,15 @@
+package SortingAlgorithm;
+
+public class BubbleSort {
+	public void BubbleSort(int[]nums) {
+		for (int i=0; i<nums.length; i++) {
+			for (int j=0; j<nums.length-i-1; j++) {
+				if (nums[j]>nums[j+1]) {
+					int temp = nums[j];
+					nums[j] = nums[j+1];
+					nums[j+1] = temp;
+				}
+			}
+		}
+	}
+}
