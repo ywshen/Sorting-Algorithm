f67a07345f4fc39b10da076e93c4f205b2b67a7c
ywshen <ywshen5002@gmail.com>
2020-05-14 15:03:05 -0400

    Revert "Adding Quick Sot"
    This reverts commit 91a6249d6df0b4f0a701f150c20264bb88fdc97f.

--- src/SortingAlgorithm/Tester.java
+++ src/SortingAlgorithm/Tester.java
@@ -5,11 +5,9 @@ public class Tester {
 	public static void main(String[] args) {
 		BubbleSort b = new BubbleSort();
 		MergeSort m = new MergeSort();
-		QuickSort q = new QuickSort();
 		int[] arr = {89, 45, 68, 90, 29, 34,  17};
 		//b.BubbleSort(arr);
-		//m.Sort(arr);
-		q.quickSort(arr, 0, arr.length-1);
+		m.Sort(arr);
 		for (int i=0; i<arr.length; i++) {
 			System.out.print(arr[i] + " ");
 		}
