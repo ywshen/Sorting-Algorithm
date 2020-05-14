9359782d245fa7ba0a254f37d6e8e882480ab85e
ywshen <ywshen5002@gmail.com>
2020-05-14 15:05:50 -0400

    Adding Quick Sort

--- src/SortingAlgorithm/Tester.java
+++ src/SortingAlgorithm/Tester.java
@@ -5,9 +5,11 @@ public class Tester {
 	public static void main(String[] args) {
 		BubbleSort b = new BubbleSort();
 		MergeSort m = new MergeSort();
+		QuickSort q = new QuickSort();
 		int[] arr = {89, 45, 68, 90, 29, 34,  17};
 		//b.BubbleSort(arr);
-		m.Sort(arr);
+		//m.Sort(arr);
+		q.quickSort(arr, 0, arr.length-1);
 		for (int i=0; i<arr.length; i++) {
 			System.out.print(arr[i] + " ");
 		}
