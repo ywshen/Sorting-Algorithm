df58b296417300e5cfa0300a1c17ae079934ba92
ywshen <ywshen5002@gmail.com>
2020-10-26 16:00:00 -0400

    Update QuickSort.java

--- src/SortingAlgorithm/QuickSort.java
+++ src/SortingAlgorithm/QuickSort.java
@@ -5,7 +5,6 @@ public class QuickSort {
 	public void quickSort(int[]arr, int low, int high) {
 		if (low<high) {
 			int pi = partition(arr, low, high);
-			
 			quickSort(arr, low, pi-1);
 			quickSort(arr, pi+1, high);
 		}
@@ -23,10 +22,9 @@ public class QuickSort {
 			}
 		}
 		int temp = arr[index+1]; 
-        arr[index+1] = arr[high]; 
-        arr[high] = temp; 
-        
-        return index+1;
+		arr[index+1] = arr[high]; 
+		arr[high] = temp; 
+		return index+1;
 	}
 
 }
